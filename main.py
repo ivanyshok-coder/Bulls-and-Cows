ca91bb006321331c79bd06928e587359513388cd
yctyPU-DOPogy <wolf261195@gmail.com>
2025-11-12 22:09:16 +0500

    Логика программы обновлена.

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import random
 
-# Генерируем 4-значное число с неповторяющимися цифрами
 digits = list("0123456789")
 random.shuffle(digits)
 rand = ''.join(digits[:4])
-
-print('*** Игра "Быки и коровы" ***')
-print('Компьютер загадал 4-значное число с разными цифрами.')
-print('Попробуйте его угадать!')
 
 count = 1
 number = input('Введите 4-значное число: ')
 
 while number != rand:
-    bulls = 0  # цифры на правильных местах
-    cows = 0   # цифры есть, но на других местах
+    bulls = 0
+    cows = 0
 
     for i in range(4):
         if number[i] == rand[i]:
@@ -26,5 +21,3 @@ while number != rand:
     count += 1
     number = input('Попробуйте ещё раз: ')
 
-print('Вы угадали число!')
-print('Количество потраченных попыток:', count)
